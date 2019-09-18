73c31f4daf07d7170903a1d7c3e7e5171a0a40e5
karanmeetkhatra10 <karanmeet.khatra@gmail.com>
2019-09-17 19:54:43 -0700

    This is probably a revolutionary method that I have created. All credits to me.

--- src/gitExercise.java
+++ src/gitExercise.java
@@ -2,9 +2,11 @@ public class gitExercise {
 
     public static void main(String args[]) {
 
-        randomPrint;
+        //randomPrint;
 
         printHi(10);
+
+        addUpTo(15);
 
     }
 
@@ -12,6 +14,14 @@ public class gitExercise {
         for (int i = 0; i < times; i++) {
             System.out.println("Hi");
         }
+    }
+
+    private static void addUpTo(int number){
+        int sum = 0;
+        for(int i = 0; i < number; i++){
+            sum += i;
+        }
+        System.out.println(sum);
     }
 
 }
